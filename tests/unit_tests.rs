866b97123c3f9dc0bf2a4110585e8fe2dea89ad0
Ashaun Thomas <ashaunthomas@siu.edu>
2022-05-17 20:13:26 -0700

    adds unit tests

--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,9 +1,25 @@
-// use file_alphabetizer;
+#[cfg(test)]
+mod unit_tests {
+    use std::path::PathBuf;
+    use file_alphabetizer::lib;
 
-// #[cfg(test)]
-// mod unit_tests {
-//     #[test]
-//     fn  should_get_content() {
-        
-//     }
-// }
+    #[test]
+    #[should_panic(expected = "File wasn't found: Os { code: 2, kind: NotFound, message: \"No such file or directory\" }")]
+    fn  should_return_error_if_file_doesnt_exist() {
+        let file = PathBuf::new();
+        lib::get_content(&file);
+    }
+
+    #[test]
+    fn should_return_file_contents() {
+        let file = PathBuf::from("src/test");
+        assert_eq!(lib::get_content(&file), "this is a file")
+    }
+
+    #[test]
+    fn should_split_content() {
+        let content = String::from("Test string");
+        let content_vec = vec!["Test", "string"];
+        assert_eq!( lib::split(&content), content_vec);
+    }
+}

d0e1824048a584f5e37ae03dc6fe3a8fba265d3c
Ashaun Thomas <ashaunthomas@siu.edu>
2022-06-05 09:03:19 -0600

    fixes failing tests by placing tests in calling context

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,58 @@
 //! A library that alphabetizes a given file.
-    use clap::{Error};
-    use std::fs;
-    use std::path::PathBuf;
-    use anyhow::{ Result, Context };
+use clap::{Error};
+use std::fs;
+use std::path::PathBuf;
+use anyhow::{ Result, Context };
 
-    fn get_content(filepath: &PathBuf) -> Result<String> {
-        let s = fs::read_to_string(filepath).with_context(|| format!("File not found: {:?}", filepath));
-        s
-    }
-    
-    fn split(content: &String) -> Vec<&str>  {
-        content.split(" ").collect()
-    }
+fn get_content(filepath: &PathBuf) -> Result<String> {
+    let s = fs::read_to_string(filepath).with_context(|| format!("File not found: {:?}", filepath));
+    s
+}
 
-    /// Alphabetizes a file given a valid file path.
-    /// 
-    /// # Examples
-    /// 
-    /// Basic usage:
-    /// 
-    /// ```
-    /// use std::{ path::PathBuf };
-    /// 
-    /// let p = PathBuf::from("src/someFileName");
-    /// file_alphabetizer::alphabetize(&p).unwrap();
-    /// 
-    /// ```
-    pub fn alphabetize(filepath: &PathBuf) -> Result<(), Error>  {
-        let content: String = get_content(filepath).unwrap();
-        let mut word_collection: Vec<&str> = split(&content);
-        word_collection.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
-        let _s = fs::write(filepath, word_collection.join(" "))
-            .with_context(|| format!("failed to write to file: {:?}", filepath));
-        Ok(())
-    }
+fn split(content: &String) -> Vec<&str>  {
+    content.split(" ").collect()
+}
+
+/// Alphabetizes a file given a valid file path.
+/// 
+/// # Examples
+/// 
+/// Basic usage:
+/// 
+/// ```
+/// use std::{ path::PathBuf };
+/// 
+/// let p = PathBuf::from("src/test");
+/// file_alphabetizer::alphabetize(&p).unwrap();
+/// 
+/// ```
+pub fn alphabetize(filepath: &PathBuf) -> Result<(), Error>  {
+    let content: String = get_content(filepath).unwrap();
+    let mut word_collection: Vec<&str> = split(&content);
+    word_collection.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+    let _s = fs::write(filepath, word_collection.join(" "))
+        .with_context(|| format!("failed to write to file: {:?}", filepath));
+    Ok(())
+}
+
+//  TESTS BELOW
+
+#[test]
+#[should_panic]
+fn  should_return_error_if_file_doesnt_exist() {
+    let file = PathBuf::new();
+    get_content(&file).unwrap();
+}
+
+#[test]
+fn should_return_file_contents() {
+    let file = PathBuf::from("src/test");
+    assert_eq!(get_content(&file).unwrap(), "this is a file")
+}
+
+#[test]
+fn should_split_content() {
+    let content = String::from("Test string");
+    let content_vec = vec!["Test", "string"];
+    assert_eq!(split(&content), content_vec);
+}
